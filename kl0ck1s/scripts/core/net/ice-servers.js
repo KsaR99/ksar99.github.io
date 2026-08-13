12d24a845fdd7a775e179a3257ebea2853412313
KsaR <KsaR99@users.noreply.github.com>
2026-08-13 03:31:45 +0200

    M.P. fixing #4

--- kl0ck1s/scripts/core/net/ice-servers.js
+++ kl0ck1s/scripts/core/net/ice-servers.js
@@ -1,11 +1,35 @@
 "use strict";
 
+const METERED_TURN_USERNAME = "c8322df6675949b6fe8d4b3d";
+const METERED_TURN_CREDENTIAL = "SWNzHmi5u+GbyQVi";
+
 export const ICE_SERVERS = Object.freeze([
     Object.freeze({urls: "stun:stun.l.google.com:19302"}),
     Object.freeze({urls: "stun:stun1.l.google.com:19302"}),
     Object.freeze({urls: "stun:stun2.l.google.com:19302"}),
     Object.freeze({urls: "stun:stun3.l.google.com:19302"}),
     Object.freeze({urls: "stun:stun4.l.google.com:19302"}),
+    Object.freeze({urls: "stun:stun.relay.metered.ca:80"}),
+    Object.freeze({
+        urls: "turn:global.relay.metered.ca:80",
+        username: METERED_TURN_USERNAME,
+        credential: METERED_TURN_CREDENTIAL,
+    }),
+    Object.freeze({
+        urls: "turn:global.relay.metered.ca:80?transport=tcp",
+        username: METERED_TURN_USERNAME,
+        credential: METERED_TURN_CREDENTIAL,
+    }),
+    Object.freeze({
+        urls: "turn:global.relay.metered.ca:443",
+        username: METERED_TURN_USERNAME,
+        credential: METERED_TURN_CREDENTIAL,
+    }),
+    Object.freeze({
+        urls: "turns:global.relay.metered.ca:443?transport=tcp",
+        username: METERED_TURN_USERNAME,
+        credential: METERED_TURN_CREDENTIAL,
+    }),
 ]);
 
 export const RTC_CONFIGURATION = Object.freeze({
